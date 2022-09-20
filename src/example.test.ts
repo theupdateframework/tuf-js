0c1b7fec98d02a46ef1dad0e6fb22fa2e81f454d
Brian DeHamer <bdehamer@github.com>
2022-09-19 18:09:01 -0700

    v2

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,42 +1,55 @@
 import canonicalize from 'canonicalize';
-import * as elliptic from 'elliptic';
+import crypto from 'crypto';
 import * as fs from 'fs';
+import { ed25519 } from './tuf-client/util/key';
 
 describe('sample verification', () => {
-  var ec = new elliptic.eddsa('ed25519');
-
   const root = JSON.parse(fs.readFileSync('./examples/root.json').toString());
   const timestamp = JSON.parse(
     fs.readFileSync('./examples/timestamp.json').toString()
   );
 
   describe('when the signature is valid', () => {
-    it('should verify', async () => {
+    it('should verify', () => {
       const signature = timestamp.signatures[0];
       const sig = signature.sig;
 
       const key = root.signed.keys[signature.keyid].keyval.public;
-      const publicKey = ec.keyFromPublic(key);
+      const publicKey = ed25519.fromHex(key);
 
       const signedDataCanonical = canonicalize(timestamp.signed) || '';
+      const te = new TextEncoder();
+      const data = te.encode(signedDataCanonical);
 
-      const result = publicKey.verify(Buffer.from(signedDataCanonical), sig);
+      const result = crypto.verify(
+        undefined,
+        data,
+        publicKey,
+        Buffer.from(sig, 'hex')
+      );
       expect(result).toBe(true);
     });
   });
 
   describe('when the signature is invalid', () => {
-    it('should NOT verify', async () => {
+    it('should NOT verify', () => {
       const signature = timestamp.signatures[0];
       const sig = signature.sig;
 
       const key = root.signed.keys[signature.keyid].keyval.public;
-      const publicKey = ec.keyFromPublic(key);
+      const publicKey = ed25519.fromHex(key);
 
       const signedDataCanonical =
         canonicalize({ ...timestamp.signed, foo: 'bar' }) || '';
+      const te = new TextEncoder();
+      const data = te.encode(signedDataCanonical);
 
-      const result = publicKey.verify(Buffer.from(signedDataCanonical), sig);
+      const result = crypto.verify(
+        undefined,
+        data,
+        publicKey,
+        Buffer.from(sig, 'hex')
+      );
       expect(result).toBe(false);
     });
   });
