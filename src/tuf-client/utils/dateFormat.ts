184e36aaa8a97d2ce91633644dde164febe17c81
eugene <ejahngithub@github.com>
2022-10-14 15:10:52 -0700

    clean up the eslint

--- src/tuf-client/utils/dateFormat.ts
+++
@@ -1,8 +0,0 @@
-export const dateToJSONFormat = (date: Date): string => {
-  console.log(
-    new Date('2021-12-18T13:28:12.990-06:00'),
-    date.toISOString(),
-    date.getTime()
-  );
-  return date.toISOString();
-};
