3ab0ee203d9f4cce8befbd3cad49864a94b1a9d7
Brian DeHamer <bdehamer@github.com>
2022-10-28 11:46:40 -0700

    implement json canonicalization (#66)

---
+++ src/utils/json.test.ts
@@ -0,0 +1,10 @@
+import { canonicalize } from './json';
+
+describe('canonicalize', () => {
+  it('should canonicalize a string', () => {
+    const json = { z: 'zee', a: 'aye', newline: 'foo\nbar' };
+    expect(canonicalize(json)).toEqual(
+      Buffer.from('{"a":"aye","newline":"foo\nbar","z":"zee"}')
+    );
+  });
+});
