f1e06c4d005d2ddcfb528d6c5231a2108e176a9a
ejahnGithub <ejahngithub@github.com>
2022-12-07 16:29:18 -0800

    reformat the test data

--- src/__tests__/__fixtures__/targets.ts
+++ src/__tests__/__fixtures__/targets.ts
@@ -1,13 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-export const targetsJson = JSON.parse(
-  fs.readFileSync(
-    path.resolve(__dirname, '../../../repository_data/metadata/targets.json'),
-    'utf-8'
-  )
-);
-
 export const role1Json = JSON.parse(
   fs.readFileSync(
     path.resolve(__dirname, '../../../repository_data/metadata/role1.json'),
@@ -19,6 +12,8 @@ export const rawTargetsJson = fs.readFileSync(
   path.resolve(__dirname, '../../../repository_data/metadata/targets.json'),
   'utf8'
 );
+
+export const targetsJson = JSON.parse(rawTargetsJson);
 
 export const rawFile1Txt = fs.readFileSync(
   path.resolve(__dirname, '../../../repository_data/targets/file1.txt'),
