d39d4de05ee97de61f07fd9cef91da068d4530ff
Brian DeHamer <bdehamer@github.com>
2022-12-29 12:25:34 -0800

    refactor downloadBytes/downloadFile
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,58 +1,73 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 import fetch from 'make-fetch-happen';
+import util from 'util';
 
 import { DownloadHTTPError, DownloadLengthMismatchError } from './error';
 import { withTempFile } from './utils/tmpfile';
 
-type DownloadFileHandler = (tmpFilePath: string) => Promise<Buffer>;
+type DownloadFileHandler<T> = (file: string) => Promise<T>;
 
 export abstract class BaseFetcher {
-  protected timeout?: number;
-
-  constructor(timeout?: number) {
-    this.timeout = timeout;
-  }
-
   abstract fetch(url: string): Promise<NodeJS.ReadableStream>;
 
-  // Download file from given ``url``.
-  public async downloadFile(
+  // Download file from given URL. The file is downloaded to a temporary
+  // location and then passed to the given handler. The handler is responsible
+  // for moving the file to its final location. The temporary file is deleted
+  // after the handler returns.
+  public async downloadFile<T>(
     url: string,
     maxLength: number,
-    handler: DownloadFileHandler
-  ): Promise<Buffer> {
-    return withTempFile(async (tmpFile, tmpFilePath) => {
+    handler: DownloadFileHandler<T>
+  ): Promise<T> {
+    return withTempFile(async (tmpFile) => {
       const reader = await this.fetch(url);
 
       let numberOfBytesReceived = 0;
+      const fileStream = fs.createWriteStream(tmpFile);
 
-      for await (const chunk of reader) {
-        const bufferChunk = Buffer.from(chunk);
-        numberOfBytesReceived += bufferChunk.length;
+      // Read the stream a chunk at a time so that we can check
+      // the length of the file as we go
+      try {
+        for await (const chunk of reader) {
+          const bufferChunk = Buffer.from(chunk);
+          numberOfBytesReceived += bufferChunk.length;
 
-        if (numberOfBytesReceived > maxLength) {
-          throw new DownloadLengthMismatchError('Max length reached');
+          if (numberOfBytesReceived > maxLength) {
+            throw new DownloadLengthMismatchError('Max length reached');
+          }
+
+          await writeBufferToStream(fileStream, bufferChunk);
         }
-        await tmpFile.write(bufferChunk);
+      } finally {
+        // Make sure we always close the stream
+        await util.promisify(fileStream.close).bind(fileStream)();
       }
-      return await handler(tmpFilePath);
+
+      return handler(tmpFile);
     });
   }
 
-  // Download bytes from given ``url``.
+  // Download bytes from given URL.
   public async downloadBytes(url: string, maxLength: number): Promise<Buffer> {
-    return await this.downloadFile(url, maxLength, async (tmpFilePath) => {
-      const readFile = await fs.open(tmpFilePath, 'r');
-      const data = await readFile.readFile();
-      readFile.close();
-      return data;
+    return this.downloadFile(url, maxLength, async (file) => {
+      const stream = fs.createReadStream(file);
+      const chunks: Buffer[] = [];
+
+      for await (const chunk of stream) {
+        chunks.push(chunk);
+      }
+
+      return Buffer.concat(chunks);
     });
   }
 }
 
 export class Fetcher extends BaseFetcher {
+  private timeout?: number;
+
   constructor(timeout?: number) {
-    super(timeout);
+    super();
+    this.timeout = timeout;
   }
 
   public override async fetch(url: string): Promise<NodeJS.ReadableStream> {
@@ -65,3 +80,17 @@ export class Fetcher extends BaseFetcher {
     return response.body;
   }
 }
+
+const writeBufferToStream = async (
+  stream: fs.WriteStream,
+  buffer: Buffer
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    stream.write(buffer, (err) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(true);
+    });
+  });
+};
