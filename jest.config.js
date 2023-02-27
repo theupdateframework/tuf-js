946b8515e073d1ae6bcf32569e39b68675a2573d
Brian DeHamer <bdehamer@github.com>
2023-02-27 11:39:19 -0800

    monorepo configuration
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/src/__tests__/__fixtures__',
-  ],
+  projects: ['<rootDir>/packages/*/jest.config.js'],
 };
